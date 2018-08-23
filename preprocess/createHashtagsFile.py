2c1edbe85f12bb3ef8197d7aef2dc388160d0156
Rachana Mane <rachanamane93@gmail.com>
2018-08-23 23:06:44 +0100

    Remember the previous hashtag write directory

--- preprocess/createHashtagsFile.py
+++ preprocess/createHashtagsFile.py
@@ -31,10 +31,12 @@ def _process_directory(dir, hashtags):
         elif isdir(filePath) and file != "extra":
             _process_directory(filePath, hashtags)
 
+
 def _write_hashtag_label_set_file(hashtags):
     hashtag_id_lookup = {}
     with open(FLAGS.hashtags_output_file, 'w') as f:
         index = 0
+        f.write("%s\n" % FLAGS.dataset_dir)
         for key in hashtags:
             f.write('%6s %s\n' % (index, key))
             hashtag_id_lookup[key] = index
@@ -45,7 +47,11 @@ def _write_hashtag_label_set_file(hashtags):
 def _read_hashtag_label_set_file():
     hashtag_id_lookup = {}
     with open(FLAGS.hashtags_output_file, 'r') as f:
+        is_first_line=True
         for line in f:
+            if is_first_line:
+                is_first_line=False
+                continue
             words = line.strip().split(" ")
             hashtag_id_lookup[words[1]] = int(words[0])
     return hashtag_id_lookup
@@ -57,6 +63,11 @@ def _create_hashtag_label_set(should_write=False):
     hashtags = dict(sorted(hashtags.items(), key=itemgetter(1), reverse=True)[:FLAGS.label_set_size])
     return _write_hashtag_label_set_file(hashtags)
 
+def _get_previous_hashtag_dir():
+    with open(FLAGS.hashtags_output_file, 'r') as f:
+        for line in f:
+            return line.strip()
+
 
 def get_hashtag_label_set(force_overwrite=False):
     """
@@ -64,6 +75,10 @@ def get_hashtag_label_set(force_overwrite=False):
     and value is a unique label ID corresponding to the hashtag
     """
     if isfile(FLAGS.hashtags_output_file) and not force_overwrite:
-        return _read_hashtag_label_set_file()
+        previous_dir = _get_previous_hashtag_dir()
+        if previous_dir == FLAGS.dataset_dir:
+            return _read_hashtag_label_set_file()
+        else:
+            return _create_hashtag_label_set(should_write=True)
     else:
         return _create_hashtag_label_set(should_write=True)
